18f6954ef241b58efb8ce964cb1814fe8df6b9e0
Colin <smithcolina@hotmail.com>
2021-01-12 22:23:28 +0000

    Add some more env vars

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,26 @@
 FROM ubuntu:bionic
 
-# Based on the Dockerfile by Manfred Touron "m@42.am"
-MAINTAINER Colin Smith <smithcolina@gmail.com>
+MAINTAINER Coin Smith <smithcolina@gmail.com>
+
+ENV DEBIAN_FRONTEND noninteractive
+ENV IC_VERSION "2.4.0-cheeky7"
 
 RUN apt-get -qq -y update && \
-    apt-get -qq -y install icecast2 python-setuptools sudo cron-apt && \
-    apt-get -y autoclean && \
-    apt-get clean && \
-    chown -R icecast2 /etc/icecast2 && \
-    sed -i 's/ -d//' /etc/cron-apt/action.d/3-download
+	apt-get -qq -y install sudo build-essential \
+		wget curl libxml2-dev libxslt1-dev \
+		libogg-dev libvorbis-dev libtheora-dev \
+		libspeex-dev python-pip && \
+	wget "https://github.com/cheekychops/icecast-kh/archive/icecast-$IC_VERSION.tar.gz" -O- | tar zxvf - && \
+	cd "icecast-kh-icecast-$IC_VERSION" && \
+	./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var && \
+	make && make install && useradd icecast && \
+	chown -R icecast /etc/icecast.xml
 
-CMD ["/start.sh"]
-EXPOSE 8000
-VOLUME ["/config", "/var/log/icecast2", "/etc/icecast2"]
+RUN pip install supervisor supervisor-stdout
 
 ADD ./start.sh /start.sh
 ADD ./etc /etc
+
+CMD ["/start.sh"]
+EXPOSE 8000
+VOLUME ["/config", "/var/log/icecast"]
--- start.sh
+++ start.sh
@@ -4,21 +4,52 @@ env
 
 set -x
 
-set_val() {
-    if [ -n "$2" ]; then
-        echo "set '$2' to '$1'"
-        sed -i "s/<$2>[^<]*<\/$2>/<$2>$1<\/$2>/g" /etc/icecast2/icecast.xml
-    else
-        echo "ERROR: setting for '$1' is missing!" >&2
-	exit
-    fi
-}
+if [ -n "$ICECAST_SOURCE_PASSWORD" ]; then
+    sed -i "s/<source-password>[^<]*<\/source-password>/<source-password>$ICECAST_SOURCE_PASSWORD<\/source-password>/g" /etc/icecast.xml
+fi
 
-set_val $ICECAST_SOURCE_PASSWORD source-password
-set_val $ICECAST_RELAY_PASSWORD  relay-password
-set_val $ICECAST_ADMIN_PASSWORD  admin-password
-set_val $ICECAST_PASSWORD        password
+if [ -n "$ICECAST_RELAY_PASSWORD" ]; then
+    sed -i "s/<relay-password>[^<]*<\/relay-password>/<relay-password>$ICECAST_RELAY_PASSWORD<\/relay-password>/g" /etc/icecast.xml
+fi
 
-set -e
+if [ -n "$ICECAST_ADMIN_PASSWORD" ]; then
+    sed -i "s/<admin-password>[^<]*<\/admin-password>/<admin-password>$ICECAST_ADMIN_PASSWORD<\/admin-password>/g" /etc/icecast.xml
+fi
 
-sudo -Eu icecast2 icecast2 -n -c /etc/icecast2/icecast.xml
+if [ -n "$ICECAST_PASSWORD" ]; then
+    sed -i "s/<password>[^<]*<\/password>/<password>$ICECAST_PASSWORD<\/password>/g" /etc/icecast.xml
+fi
+
+if [ -n "$ICECAST_LOCATION" ]; then
+    sed -i "s/<location>[^<]*<\/location>/<location>$ICECAST_LOCATION<\/location>/g" /etc/icecast.xml
+fi
+
+if [ -n "$ICECAST_HOSTNAME" ]; then
+    sed -i "s/<hostname>[^<]*<\/hostname>/<hostname>$ICECAST_HOSTNAME<\/hostname>/g" /etc/icecast.xml
+fi
+
+if [ -n "$ICECAST_ADMIN" ]; then
+    sed -i "s/<admin>[^<]*<\/admin>/<admin>$ICECAST_ADMIN<\/admin>/g" /etc/icecast.xml
+fi
+
+if [ -n "$ICECAST_MAX_SOURCES" ]; then
+    sed -i "s/<sources>[^<]*<\/sources>/<sources>$ICECAST_MAX_SOURCES<\/sources>/g" /etc/icecast.xml
+fi
+
+if [ -n "$ICECAST_MAX_LISTENERS" ]; then
+    sed -i "s/<clients>[^<]*<\/clients>/<clients>$ICECAST_MAX_LISTENERS<\/clients>/g" /etc/icecast.xml
+fi
+
+if [ -n "$ICECAST_X_FORWARDED_FOR" ]; then
+    sed -i "s/<x-forwarded-for><\/x-forwarded-for>/<x-forwarded-for>$ICECAST_X_FORWARDED_FOR<\/x-forwarded-for>/g" /etc/icecast.xml
+fi
+
+if [ -n "$ICECAST_LOG_LEVEL" ]; then
+    sed -i "s/<loglevel>3<\/loglevel>/<loglevel>$ICECAST_LOG_LEVEL<\/loglevel>/g" /etc/icecast.xml
+fi
+
+
+cat /etc/icecast.xml
+
+chown -R icecast /var/log/icecast
+supervisord -n -c /etc/supervisord.conf
